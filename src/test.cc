87638ac393337118c755704e8755706d3b1af92d
middzwb <zwb843536229@gmail.com>
2021-07-08 10:55:19 +0800

    update test

--- src/test.cc
+++ src/test.cc
@@ -6,76 +6,17 @@
 #include <thread>
 #include <chrono>
 #include <future>
+#include <random>
 
 using boost::system::error_code;
 namespace asio = boost::asio;
 
-template <typename Token>
-auto async_meaning_of_life(bool success, Token&& token)
-{
-#if BOOST_VERSION >= 106600
-    using result_type = typename asio::async_result<std::decay_t<Token>, void(error_code, int)>;
-    typename result_type::completion_handler_type handler(std::forward<Token>(token));
-
-    result_type result(handler);
-#else
-    typename asio::handler_type<Token, void(error_code, int)>::type
-                 handler(std::forward<Token>(token));
-
-    asio::async_result<decltype (handler)> result (handler);
-#endif
-
-    if (success)
-        handler(error_code{}, 42);
-    else
-        handler(asio::error::operation_aborted, -2);
-
-    return result.get ();
-}
-
-void using_yield_ec(asio::yield_context yield) {
-    for (bool success : { true, false }) {
-        boost::system::error_code ec;
-        auto answer = async_meaning_of_life(success, yield[ec]);
-        std::cout << __FUNCTION__ << ": Result: " << ec.message() << "\n";
-        std::cout << __FUNCTION__ << ": Answer: " << answer << "\n";
-    }
-}
-
-void using_yield_catch(asio::yield_context yield) {
-    for (bool success : { true, false }) 
-    try {
-        auto answer = async_meaning_of_life(success, yield);
-        std::cout << __FUNCTION__ << ": Answer: " << answer << "\n";
-    } catch(boost::system::system_error const& e) {
-        std::cout << __FUNCTION__ << ": Caught: " << e.code().message() << "\n";
-    }
-}
-
-void using_future() {
-    for (bool success : { true, false }) 
-    try {
-        auto answer = async_meaning_of_life(success, asio::use_future);
-        std::cout << __FUNCTION__ << ": Answer: " << answer.get() << "\n";
-    } catch(boost::system::system_error const& e) {
-        std::cout << __FUNCTION__ << ": Caught: " << e.code().message() << "\n";
-    }
-}
-
-void using_handler() {
-    for (bool success : { true, false })
-        async_meaning_of_life(success, [](error_code ec, int answer) {
-            std::cout << "using_handler: Result: " << ec.message() << "\n";
-            std::cout << "using_handler: Answer: " << answer << "\n";
-        });
-}
 
 template<typename CompletionToken>
 auto async_operate(CompletionToken token) {
-    boost::asio::async_completion<CompletionToken, void(error_code, std::string)> init(token);
+    asio::async_completion<CompletionToken, void(error_code, std::string)> init(token);
     
-    std::cout << __func__ << " this_thread id=" << std::this_thread::get_id() << std::endl;
-    // asio::async_completion<asio::yield_context, void()> init(token);
+    std::cout << __func__ << " thread id=" << std::this_thread::get_id() << std::endl;
 
     // auto op = asio::bind_executor(ex, [](boost::system::error_code ec, std::string s) {
     //     std::cout << s << std::endl;
@@ -83,10 +24,10 @@ auto async_operate(CompletionToken token) {
     // op(boost::system::error_code(), "aaps");
     
     // auto handler = init.completion_handler;
-    // static std::thread tr1([&handler]() {
     std::thread tr1([handler = init.completion_handler]() {
-        std::cout << "thread tr1 id=" << std::this_thread::get_id() << std::endl;
+        std::cout << "tr1 thread id=" << std::this_thread::get_id() << std::endl;
         using namespace std::chrono_literals;
+        std::mt19937 mt{std::random_device{}()};
         std::this_thread::sleep_for(3s);
         auto ex = asio::get_associated_executor(handler);
         asio::dispatch(ex, [handler, ex]() mutable {
@@ -100,38 +41,58 @@ auto async_operate(CompletionToken token) {
     return init.result.get();
 }
 
-void async_op(asio::yield_context yield) {
-    using namespace std::chrono;
-    auto start = system_clock::now();
-    boost::system::error_code ec;
-    auto s = async_operate(yield[ec]);
-    auto delta = system_clock::now() - start;
-    std::cout << s << " elapsed " << duration_cast<milliseconds>(delta).count() <<  std::endl;
-}
+class AsyncTest {
+public:
+    asio::io_context context_;
+    using Executor = asio::io_context::executor_type;
+    asio::executor_work_guard<Executor> work_;
+    using Signature = void(boost::system::error_code);
+    std::thread thread_;
+    asio::yield_context* yield_;
+    // asio::executor_binder<std::function<Signature>, asio::io_context>* hh;
+
+    AsyncTest()
+        :work_(asio::make_work_guard(context_))
+    {
+        thread_ = std::move(std::thread{[this](){ context_.run(); }});
+    }
+    ~AsyncTest() {
+        thread_.join();
+    }
+
+    template<typename ExecutionContext, typename CompletionToken>
+    auto async_wait(ExecutionContext&& ctx, CompletionToken&& token) {
+        std::cout << __func__ << std::endl;
+        asio::async_completion<CompletionToken, void(boost::system::error_code)> init(token);
+        auto& handler = init.completion_handler;
+        auto ex = asio::get_associated_executor(handler);
+        auto h = asio::bind_executor(ex, std::move(handler));
+        std::thread tr{[&]() {
+            using namespace std::chrono_literals;
+            std::this_thread::sleep_for(3s);
+            asio::post(ex, std::move(h));
+        }};
+        tr.detach();
+        std::cout << __func__ << " end" << std::endl;
+        return init.result.get();
+    }
+    void test_async_op() {
+        spawn(context_, [this](asio::yield_context yield) {
+            using namespace std::chrono;
+            yield_ = &yield;
+            auto start = system_clock::now();
+            boost::system::error_code ec;
+            auto s = async_operate(yield[ec]);
+            auto delta = system_clock::now() - start;
+            std::cout << s << " elapsed " << duration_cast<milliseconds>(delta).count() <<  std::endl;
+            work_.reset();
+        });
+    }
+};
 
 
 int main() {
-    // asio::io_service svc;
-
-    // spawn(svc, using_yield_ec);
-    // spawn(svc, using_yield_catch);
-    // std::thread work([] {
-    //         using_future();
-    //         using_handler();
-    //     });
-
-    // svc.run();
-    // work.join();
-    using namespace std;
-    asio::io_context ioc;
-    spawn(ioc, async_op);
-    using Executor = asio::io_context::executor_type;
-    asio::executor_work_guard<Executor> work(asio::make_work_guard(ioc));
-    std::thread tr([&ioc]() {
-        ioc.run();
-    });
-    this_thread::sleep_for(5s);
-    work.reset();
-    tr.join();
+    AsyncTest at;    
+    at.test_async_op();
 }
 
